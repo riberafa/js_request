954445ce3028898d3f2968c8e62d1335a94f77ad
Rafael Ribeiro <rafael_oliribeiro@yahoo.com.br>
2023-04-09 17:47:21 -0300

    feature: addinal final html/css/js structure of the project

---
+++ scripts.js
@@ -0,0 +1,6 @@
+function selectSecret() { 
+    const blur = document.querySelector('#blurEffect');
+    const modal = document.querySelector('#secretModalInfos');
+    blur.classList.toggle('activated')
+    modal.classList.toggle('activated')
+}
